1fe65472f5c2bc6ab8d06c68e698622181d46f8c
Aakankshap16 <aakankshap971@gmail.com>
2023-03-28 22:09:05 +0530

    call peek and pop method till empty

--- StackWithLinkedList.cs
+++ StackWithLinkedList.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace day14_StackAndQueuesProgram
+﻿namespace day14_StackAndQueuesProgram
 {
     public class StackWithLinkedList
     {
@@ -17,11 +11,11 @@ namespace day14_StackAndQueuesProgram
         public void push(int value)
         {
             Node node = new Node(value);
-            if (this.top == null) 
+            if (this.top == null)
             {
                 node.next = null;
             }
-            else 
+            else
             {
                 node.next = this.top;
             }
@@ -33,14 +27,44 @@ namespace day14_StackAndQueuesProgram
         public void Display()
         {
             Node temp = this.top;
-            while (temp != null) 
+            while (temp != null)
             {
                 Console.WriteLine(temp.data + " ");
                 temp = temp.next;
             }
         }
+        //Peek top most element
+        public void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine($"{this.top.data} is the top of the stack ");
+        }
 
+        //pop delete elements one at a tym
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty,Deletion is not possible");
+                return;
+            }
+            Console.WriteLine($"Value poped is {this.top.data}");
+            this.top = this.top.next;
+        }
 
+        // Check Empty
+        public void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+        }
 
     }
 }
