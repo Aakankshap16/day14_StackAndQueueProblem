b7ddca3ceadd679be35bdee331edfa3c71664a01
Aakankshap16 <aakankshap971@gmail.com>
2023-03-28 19:00:45 +0530

    Greeting msg

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace day14_StackAndQueuesProgram
+{
+ public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack And Queue Program!");
+        }
+
+    }
+}
