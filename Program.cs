57921eb0c8149492e6c67e4249cf2a11fd291d5e
Aakankshap16 <aakankshap971@gmail.com>
2023-03-28 19:04:20 +0530

    push element to stack call UC1

--- Program.cs
+++ Program.cs
@@ -6,6 +6,13 @@ namespace day14_StackAndQueuesProgram
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack And Queue Program!");
+            Console.WriteLine("#####################");
+
+            StackWithLinkedList obj = new StackWithLinkedList();
+            obj.push(70);
+            obj.push(30);
+            obj.push(56);
+            obj.Display();
         }
 
     }
